84f0c81284944b3bbfd090dc866d58c9121a2500
kamran <kamran2727@gmail.com>
2018-06-03 23:47:39 +0430

    I forgot to add reg.v git add reg.v !

--- reg.v
+++ reg.v
@@ -8,3 +8,15 @@ module register  #(parameter integer size = 32)(clock, reset, regIn, regOut);
       else regOut <= regIn;
     end
 endmodule
+
+
+module registerWitEnb  #(parameter integer size = 32)(clock, reset, enable, regIn, regOut);
+    input clock , reset, enable;
+    input [size - 1:0] regIn;
+    output reg [size - 1: 0] regOut;
+    
+    always @(posedge clock) begin
+      if (reset) regOut <= 0;
+      else if ( enable) regOut <= regIn;        
+    end
+endmodule
