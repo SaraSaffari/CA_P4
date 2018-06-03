fdec63d78c033fbdcbdf10267401c72394afa98d
kamran <kamran2727@gmail.com>
2018-06-03 23:48:17 +0430

    stage1 bugfix

--- stage1.v
+++ stage1.v
@@ -1,20 +1,33 @@
-module stage1(clk, rst, jmp, pcOut);
-	input jmp;
+module stage1(clk, rst, pcAdderInputBSel, pcOut, disp, pcInputSel, stackOutput, jumpAdr, instruction);
+	input pcAdderInputBSel;
 	input clk, rst;
 	output [11:0] pcOut;
-	
-	mux_2_input  #(.WORD_LENGTH (12)) pcAdderInputAmux (
-		.in1(12'd4), 
-		.in2(offset), 
-		.sel(jmp), 
+	wire [11:0] pcIn;
+	input [11:0] disp;
+	input [1:0] pcInputSel;
+	input [11:0] stackOutput;
+	input [11:0] jumpAdr;
+	output [18:0] instruction;
+	wire [11:0] pcAdderResult, pcAdderInputA;
+	mux_2_input  #(.WORD_LENGTH (12)) pcAdderInputAmux (    //mux 1
+		.in1(disp), 
+		.in2(12'd1), 
+		.sel(pcAdderInputBSel), 
 		.out(pcAdderInputA)
 	);
-
 
 	adder #(.size(12)) pcAdder(
 		.inputA(pcAdderInputA),
 		.inputB(pcOut),
-		.result(pcIn)
+		.result(pcAdderResult)
+	);
+
+	mux_3_input #(.WORD_LENGTH(12)) pcInputMux (     // mux 2
+		.in1(pcAdderResult), 
+		.in2(stackOutput), 
+		.in3(jumpAdr), 
+		.sel(pcInputSel), 
+		.out(pcIn)
 	);
 
 	register #(.size(12)) pc(
@@ -29,4 +42,5 @@ module stage1(clk, rst, jmp, pcOut);
 		.address(pcOut), 
 		.instruction(instruction)
 	);
+
 endmodule
