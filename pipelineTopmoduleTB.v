f409f2c532bfab4bb6f4a3a136c18d168e3eeec7
kamran <kamran2727@gmail.com>
2018-06-03 23:44:56 +0430

    pipelineTopmoduleTb

---
+++ pipelineTopmoduleTB.v
@@ -0,0 +1,18 @@
+module AAAAAAAAAATB();
+
+  reg clk = 0;
+  reg init = 0;
+  reg rst = 1;
+  
+  initial repeat (15) #3 init = ~init;
+  initial repeat (400) #70 clk = ~clk;
+
+  initial begin
+    #100
+    rst = 0;
+  end
+
+	pipeline p(clk, rst, init);
+	
+endmodule
+
